19b455279fab3b5321e3f9849e9926ad6c69ecb7
Danielbb21 <50447725+Danielbb21@users.noreply.github.com>
2025-11-24 12:03:16 -0300

    ✨ feat: Adding cronJob to execute code in one hour interval

--- colector/src/main.py
+++ colector/src/main.py
@@ -1,8 +1,28 @@
 import geocoder
 from dataColector import colect_data
+from datetime import datetime
+from croniter import croniter
+import time
 
+expressao_cron = "0 * * * *"  # roda todo minuto
+
+def executar_tarefa():
+    g = geocoder.ip('me')
+    print("Localização:", g.latlng)
+    colect_data(g.lat, g.lng)
 
 if __name__ == "__main__":
-    g = geocoder.ip('me')
-    print(g.latlng)
-    colect_data(g.lat, g.lng)
+    print("🔥 O cron está rodando dentro do container!")
+
+    cron = croniter(expressao_cron, datetime.now())
+
+    while True:
+        proximo = cron.get_next(datetime)
+        agora = datetime.now()
+
+        espera = (proximo - agora).total_seconds()
+        if espera > 0:
+            time.sleep(espera)
+
+        print("⏰ Executando tarefa do cron:", datetime.now())
+        executar_tarefa()
